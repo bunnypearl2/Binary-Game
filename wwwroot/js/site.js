ec3bfec089ffb38466d55524d2ce3ac13fce24d2
Megan Dobos <bunnypearl2@gmail.com>
2025-02-27 11:30:52 -0500

    Changes to navigation and adding JS references

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+window.moveNext = (event, nextId) => {
+    if (event.key === "Enter") {
+        document.getElementById(nextId)?.focus();
+        event.preventDefault(); // Prevents form submission
+    }
+};
+
+
+window.focusInput = (id) => {
+    document.getElementById(id)?.focus();
+};
+
+//Utilized in Other.razor to test JS functions
+//JS functions help for smoother input and use on mobile
