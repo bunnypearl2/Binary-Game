11b5f07f09cdea3fec88b83b0b403124e5b000ec
Megan Dobos <bunnypearl2@gmail.com>
2025-03-06 14:23:25 -0500

    Added example implementation of JavaScript Interlop in Other.razor. Implemented autotab capability in Counter.razor (will want to duplicate this any time we take numeric input from user).  But I'll do further testing with this in the current page it's in.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,47 @@
 ﻿
+//document.addEventListener("DOMContentLoaded", function () {
+//    if (window.initializeJsFunction) {
+//        window.initializeJsFunction();
+//    }
+//});
+
+//window.initializeJsFunction = function () {
+//    console.log("JavaScript initialized after Blazor.");
+//};
+
+// focusElement used for autotab - current example in Counter.razor
+window.focusElement = function (elementId) {
+    let nextElement = document.getElementById(elementId);
+    if (nextElement) {
+        nextElement.focus();
+    }
+};
+
+
+
+// THE FOLLOWING ARE NOT CURRENTLY USED
+window.moveFocusTo = function (nextElementId) {
+    let nextElement = document.getElementById(nextElementId);
+    if (nextElement) {
+        nextElement.focus();
+    }
+};
+
+window.removeFocus = function (elementId) {
+    let element = document.getElementById(elementId);
+    if (element) {
+        element.blur(); // Removes focus
+    }
+};
+
+
+window.selectText = (element) => {
+    if (element) {
+        element.select();  // Selects the text in the input element
+    }
+};
+
+
 window.moveNext = (event, nextId) => {
     if (event.key === "Enter") {
         document.getElementById(nextId)?.focus();
@@ -11,5 +54,8 @@ window.focusInput = (id) => {
     document.getElementById(id)?.focus();
 };
 
+
+
+
 //Utilized in Other.razor to test JS functions
-//JS functions help for smoother input and use on mobile
+//JS functions help for smoother input 
