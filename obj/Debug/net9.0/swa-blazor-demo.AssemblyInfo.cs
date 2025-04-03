490028d5295e25322ca16ab3bda977d725dadc4b
Inademite <violinman2011@gmail.com>
2025-04-03 13:30:44 -0400

    Adding Endian Page

--- obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5aa9085c5b6116da5b5839a4608808f0876dd30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb6d15f76faed5789f6cdb9ac9870721173c544")]
 [assembly: System.Reflection.AssemblyProductAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
