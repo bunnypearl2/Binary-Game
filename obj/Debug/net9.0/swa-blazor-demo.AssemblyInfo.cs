81726bbda032049f3492e4f81834869db49918ae
Megan Dobos <bunnypearl2@gmail.com>
2025-04-03 15:39:48 -0400

    Changed page name to "Converter"

--- obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154c0ea544ea65444924cd011e7595bbf0c1e50b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0599c91f6c5349133e7f8f256f75b1785697d55a")]
 [assembly: System.Reflection.AssemblyProductAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
