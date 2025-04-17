62763b968e03cee1f658dc93553ca777a478ab84
Inademite <violinman2011@gmail.com>
2025-04-17 13:08:52 -0400

    Updating Local Repo

--- obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
+++ obj/Debug/net9.0/swa-blazor-demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addcddca543183ddaa0b9ec0fcd22df28e2140ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c466aba28b6804ed476eb85dcf1f3851a741263")]
 [assembly: System.Reflection.AssemblyProductAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("swa-blazor-demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
